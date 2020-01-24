e96e3fb7bd7f21b9958cb22289c799055f2aa15f
John Garvin <john.garvin@gmail.com>
2020-01-23 17:32:51 -0700

    Initial Commit

---
+++ PhotoList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PhotoList
+//
+//  Created by John Garvin on 2020-01-23.
+//  Copyright © 2020 John Garvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
